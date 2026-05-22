d5b83b38b5a53c60c6cd99e8dee9e010305140da
elmarco008 <garciacibrianmarco08@gmail.com>
2026-05-22 10:17:41 +0200

    .

--- back/database/fix_passwords.sql
+++ back/database/fix_passwords.sql
@@ -1,14 +1,14 @@
 -- ============================================================
 -- fix_passwords.sql
 -- Actualiza los password_hash de todos los usuarios del seed
--- con un hash bcrypt válido de la contraseña: GlobalGemba2024!
+-- con un hash bcrypt válido de la contraseña: GlobalGemba2026!
 -- 
 -- EJECUTAR EN psql:
 --   \i database/fix_passwords.sql
 -- ============================================================
 
 UPDATE usuarios
-SET password_hash = '$2a$12$tZycAZui65r2jKDaywnC0eK16shp0dFnvl8VqZEM9a8ZG0gqfuvZ2'
+SET password_hash = '$2a$12$3nr/G1LKpV8kHZFxqpDTwOsM2It.W20hhvr27iWwRRWiHHbSA.k0C'
 WHERE email IN (
     'admin@globalgemba.com',
     'carlos.rodriguez@globalgemba.com',
